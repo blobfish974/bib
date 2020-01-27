fb0411bd5f352f73c6a760d4ba1ddc45b45cc998
Tassan-Mazzocco Arthur <arthur.tassan@gmail.com>
2020-01-27 23:41:41 +0100

    finish function to store all bib restaurants url in a json file

--- server/index.js
+++ server/index.js
@@ -10,76 +10,88 @@ const readline = require('readline'); //to readfiles / import datas
 
 async function fetchPageBibList(url){
 const res=await sandbox_package.sandboxPageBibList(); 
-console.log("pages2="+res);
 return res;
 }
 
-function getNumberOfPage(){
-  const promise1= new Promise((resolve, reject) => {
-    const r=fetchPageBibList();
-    return r;
-
-  })
-  promise1.then(function(value) {
-  console.log(value);
-  // expected output: "foo"
-});
-  return n;
-}
 
 
 
-async function fetchBibList(url){
-// const res=await sandbox_package.sandboxBibList(url); 
-const res=await sandbox_package.sandboxPageBibList(url); 
-console.log(res);}
-
-function bibUrlList(){
+async function bibUrlList(){ //fetch all the url from restaurant wich are bib and store them in a json file
 	// const numberOfResults=fetchPageBibList('https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/');
-	const numberOfResults=await sandbox_package.sandboxPageBibList(); 
+	const numberOfResults=await sandbox_package.sandboxPageBibList();  
 	const numberOfPage=Math.floor(numberOfResults/20);
 	console.log("pages="+numberOfPage);
-	var url="https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/";
-	var i;
-	for (i = 1; i < 3; i++) {
-		console.log(url.concat(i.toString()));
-  		// fetchBibList(url.concat(' ', i.toString()));
+	const urlBeginning="https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/";
+	var i,url,restaurant, restaurantURL;
+	var bibURLList=[];
+	for (i = 1; i < (numberOfPage+2); i++) {
+	// for (i = 1; i < 2; i++) {
+		// console.log(url.concat(i.toString()));
+		url=urlBeginning.concat(i.toString());
+		// console.log(url);
+		restaurantURL= await sandbox_package.sandboxBibList(url);
+		// bibURLList.push(restaurantURL.web_link);
+		bibURLList=bibURLList.concat(restaurantURL.web_link);
 	}
+	// console.log(bibURLList);
+
+
+	// export in JSON
+	fs.writeFile('./files/BibRestaurantsURL.json', JSON.stringify(bibURLList,null,2), (err) => {
+	    if (err) throw err;
+	    console.log('Data written to file');
+	});
+
+
+	// fs.writeFile("./files/BibRestaurantsURL.txt", bibURLList, function(err) {
+ //    if(err) {
+ //        return console.log(err);
+ //    }
+ //    console.log("The file was saved!");
+	// }); 
+
+
+
+
+	//Now let's fetch all the url one by one
+	// for (i = 1; i < 3; i++) {
+	// 	url=;
+	// 	restaurant=await sandbox_package.sandboxRestaurant(url); 
+ //  		bibList.push(restaurant);
+		
+	// }
+	
+	// console.log(bibList);
 	
 }
-const r=fetchPageBibList('r');
-console.log("TEST"+r);
-const j=getNumberOfPage();
-console.log("J="+j);
+
 // fetchPageBibList();
-// bibUrlList();
+bibUrlList();
 
 
 
-// let student = { 
-//     name: 'Mike',
-//     age: 23, 
-//     gender: 'Male',
-//     department: 'English',
-//     car: 'Honda' 
-// };
- 
-// // let data = JSON.stringify(student);
-// let data = JSON.stringify(student, null, 2);
 
-// // fs.writeFileSync('student-2.json', data);
-// fs.writeFile('student-3.json', data, (err) => {
-//     if (err) throw err;
-//     console.log('Data written to file');
+
+
+// SOME TESTS:
+
+// function getNumberOfPage(){
+//   const promise1= new Promise((resolve, reject) => {
+//     const r=fetchPageBibList();
+//     return r;
+
+//   })
+//   promise1.then(function(value) {
+//   console.log(value);
+//   // expected output: "foo"
 // });
+//   return n;
+// }
+// const r=fetchPageBibList('r');
+// console.log("TEST"+r);
+// const j=getNumberOfPage();
+// console.log("J="+j);
 
-// console.log('This is after the write call');
 
 
 
-// fs.writeFile("/files/BibRestaurantsURL", "Hey there!", function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//     console.log("The file was saved!");
-// }); 
